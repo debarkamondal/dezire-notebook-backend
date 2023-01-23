08412b23b3a625066434f533b25e3091dcfc5f53
debarkamondal <debarkamondal@gmail.com>
2023-01-23 12:29:10 +0530

    connected to mongoDB and used express

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoUri ="mongodb://localhost:27017"
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoUri,()=>{
+        console.log("connected to db successfully");
+    })
+}
+module.exports = connectToMongo;
