436e709c05a065da69a80eea08e33b885b2b5ee7
debarkamondal <debarkamondal@gmail>
2023-01-28 01:23:12 +0530

    re: using async instaead of promises

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ const mongoUri ="mongodb://127.0.0.1:27017/dezire-notebook"
 
 const connectToMongo=()=>{
     mongoose.connect(mongoUri,(err)=>{
+        if(err){
         console.log(err);
+        }
+        else{
+            console.log("Conntected to MongoDB")
+        }
     })
 }
 module.exports = connectToMongo;
--- routes/auth.js
+++ routes/auth.js
@@ -5,24 +5,36 @@ const { body, validationResult } = require("express-validator");
 
 //Create user using : POST at /api/auth
 router.post(
-  "/",
+  "/createuser",
   [
     body("email", "Enter a valid email").isEmail(),
     body("password", "Length must be atleast 5").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+
+    //If there are errors return bad request
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    //   res.send(req.body);
+      //   res.send(req.body);
     }
-    User.create({
+
+    //checking if the user already exist
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        console.log(user);
+        return res.status(400).json({ error: req.body.email });
+      }
+      user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=> {console.log(err);
-    res.json({error: "Please enter an unique email"})})
-  }
+      });
+      res.json(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some error occured")
+    }
 );
 module.exports = router;
