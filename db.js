54c7e6675ba1565322a3958244bd73d149d45398
debarkamondal <debarkamondal@gmail>
2023-01-28 00:40:36 +0530

    fix: added validation for user creation

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri ="mongodb://127.0.0.1:27017"
+const mongoUri ="mongodb://127.0.0.1:27017/dezire-notebook"
 
 const connectToMongo=()=>{
     mongoose.connect(mongoUri,(err)=>{
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,28 @@
-const express= require('express');
-const User= require('../models/Users')
+const express = require("express");
+const User = require("../models/Users");
 const router = express.Router();
-// const { Schema } = mongoose;
-
+const { body, validationResult } = require("express-validator");
 
 //Create user using : POST at /api/auth
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user= User(req.body);
-    user.save();
-    res.send(req.body)
-})
-module.exports = router
+router.post(
+  "/",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Length must be atleast 5").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    //   res.send(req.body);
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      }).then(user => res.json(user))
+      .catch(err=> {console.log(err);
+    res.json({error: "Please enter an unique email"})})
+  }
+);
+module.exports = router;
