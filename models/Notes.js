1bc5d37f9926cffab78c2c04d4746c0cd426c82d
debarkamondal <debarkamondal@gmail>
2023-01-29 18:58:00 +0530

    feat: added endpoint for saving note

--- models/Notes.js
+++ models/Notes.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    tag:{
-        type: String,
-        default: "Genearl"
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
-module.exports= mongoose.model('notes', NotesSchema)
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    default: "Genearl",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("notes", NotesSchema);
--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,46 @@
-const express= require('express');
+const express = require("express");
 const router = express.Router();
+const fetchUser = require("../middleware/fetchUser");
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res)=>{
-    res.json([])
-})
-module.exports = router
+//---------------------------------------------Fetching all notes for user-------------------------------------------------//
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Some error occured");
+  }
+});
+
+//-----------------------------------------------------Adding new note-----------------------------------------------------//
+
+router.post(
+  "/addnote",
+  fetchUser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be a bit longer").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    const { title, description, tag } = req.body;
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      //creating new note in db collection
+      const note = new Notes({ title, description, tag, user: req.user.id });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+);
+
+module.exports = router;
