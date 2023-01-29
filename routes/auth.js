91a0ce7a1f6718f67f05b281274d9b8221145f3d
debarkamondal <debarkamondal@gmail>
2023-01-29 09:22:17 +0530

    fe: added login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "dezirenotebook";
 
-//Create user using : POST at /api/auth
+//Create user using : POST at /api/auth/createuser
 router.post(
   "/createuser",
   [
@@ -27,7 +27,7 @@ router.post(
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         console.log(user);
-        return res.status(400).json({ error: req.body.email });
+        return res.status(400).send("User already exists!!!");
       }
 
       // Generating salt and hashing password for storage
@@ -41,13 +41,46 @@ router.post(
         email: req.body.email,
       });
 
-      const data={
-        user:{id: user.id}
-      }
-      const authToken=jwt.sign(data, JWT_SECRET);
-      console.log(authToken);
+      // creating auth token to cache login
+      const data = {
+        user: { id: user.id },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
 
-      res.json({authToken});
+      res.json({ authToken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some error occured");
+    }
+  }
+);
+
+// Loggin in user at /api/login
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Length must be atleast 5").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    //If there are errors return bad request
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //checking if the user already exist
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      //checking if password matches
+      if (user.password == req.body.password) {
+        const data = {
+          user: { id: user.id },
+        };
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+      } else res.json({ error: "Wrong credentials" });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Some error occured");
