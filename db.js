ee72340db5451e8f70e866c6e8ace8edf927d825
debarkamondal <debarkamondal@gmail>
2023-03-01 01:00:44 +0530

    re: indentation fix

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
-const mongoUri ="mongodb://127.0.0.1:27017/dezire-notebook"
+const mongoose = require("mongoose");
+const mongoUri = "mongodb://127.0.0.1:27017/dezire-notebook";
 
-const connectToMongo=()=>{
-    mongoose.connect(mongoUri,(err)=>{
-        if(err){
-        console.log(err);
-        }
-        else{
-            console.log("Conntected to MongoDB")
-        }
-    })
-}
+const connectToMongo = () => {
+  mongoose.connect(mongoUri, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Conntected to MongoDB");
+    }
+  });
+};
 module.exports = connectToMongo;
